21b9dab12e75caa6b3ba618101d8a920c447f3cb
Tommi Ylimäki <tommi.ylimaki@seamk.fi>
2023-03-30 13:45:34 +0300

    Pick host ip automatically to html template

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, send_from_directory, send_file
+from flask import Flask, render_template, Response, send_from_directory, send_file, request
 import threading
 import configparser
 import time
@@ -12,13 +12,17 @@ port = config.getint('web', 'port')
 template_dir = os.path.abspath('webMQTT')
 static_dir = os.path.abspath('webMQTT')
 print(template_dir)
+
 app = Flask(__name__, template_folder = template_dir, static_folder = static_dir)
+
 
 @app.route('/')
 def index_page():
     """Video streaming home page."""
-    return render_template('index.html')
+    host = request.headers.get('Host').split(":")[0]
+    return render_template('index.html', host=host)
     #return 'web app with python flask!'
+
 
 def main():
     start_web = threading.Thread(target = app.run, kwargs={'host':'0.0.0.0', 'port':port})
